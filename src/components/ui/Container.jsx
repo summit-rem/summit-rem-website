41d53a52c577e396cf779fa99a3324216aa7ad2f
Shabbir Umrali <shabbirumrali@gmail.com>
2025-12-31 02:05:35 +0530

    Updated - Container.jsx with css change

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Container({ className = "", as: Comp = "div", children }) {
   return (
-    <Comp className={`mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`}>
+    <Comp className={`mx-auto w-full px-4 sm:px-6 lg:px-8 ${className}`}>
       {children}
     </Comp>
   );
