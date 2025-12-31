c653d8a7530a3e351e02dc03e77f63182b5cb973
Shabbir Umrali <shabbirumrali@gmail.com>
2025-12-31 23:13:41 +0530

    Updated - servicesNav.js making it capital letters

--- src/pages/services/serviceNav.js
+++ src/pages/services/serviceNav.js
@@ -5,9 +5,9 @@ export const serviceNav = [
   { label: "ACCOUNTING & FINANCIAL REPORTING", to: "/accounting-financial-reporting" },
   { label: "LEASE MANAGEMENT", to: "/lease-management" },
   { label: "REAL ESTATE ADVISORY SERVICES", to: "/real-estate-advisory-services" },
-  { label: "Maintenance & Capital Projects", to: "/maintenance-and-capital-projects" },
-  { label: "Owners’ Association Management", to: "/owners-association-management" },
-  { label: "Mobile Home Park Management", to: "/mobile-Home-park-management" },
+  { label: "MAINTENANCE & CAPITAL PROJECTS", to: "/maintenance-and-capital-projects" },
+  { label: "OWNERS’ ASSOCIATION MANAGEMENT", to: "/owners-association-management" },
+  { label: "MOBILE HOME PARK MANAGEMENT", to: "/mobile-Home-park-management" },
 
   // { label: "TENANT SERVICES & SAFETY", to: "/tenant-services-and-safety" },
   // { label: "TRANSITION STRATEGY", to: "/transition-strategy" },
